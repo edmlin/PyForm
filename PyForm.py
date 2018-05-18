92d757307d90030e96bb4b37669bcbc430cce1f6
Edmund Lin <edm.lin@gmail.com>
2018-05-18 15:36:15 -0400

    Mutiline textbox

--- PyForm.py
+++ PyForm.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import pygame.gfxdraw
 
 
 def render_text_pos(screen, text, font, pos, color, background=None):
@@ -81,7 +82,7 @@ class Control:
 class Label(Control):
     def __init__(self, form=None, left=0, top=0, width=0, height=0, text=""):
         super().__init__(form, left, top, width, height)
-        self.text = text
+        self.text = text.replace("\r\n", "\n")
         self.color = pg.Color('black')
         self.background = pg.Color('white')
         self.surface = None
@@ -123,102 +124,183 @@ class Button(Label):
 
 
 class TextBox(Label):
-    def __init__(self, form=None, left=0, top=0, width=0, height=0, text=""):
+    def __init__(self, form=None, left=0, top=0, width=0, height=0, text="", multilines=False):
         super().__init__(form, left, top, width, height, text)
         self.padding_left = 2
-        self.padding_top = 0
-        self.caret_pos = len(text)
-        self.first_visible = 0
+        self.padding_top = 1
+        self.caret_row = 0
+        self.caret_col = 0
+        self.first_visible_col = 0
+        self.first_visible_row = 0
+        self.multilines = multilines
+        self.current_line = 0
+
+    def get_lines(self):
+        return self.text.split("\n")
 
     def caret_x(self):
-        text = self.text[self.first_visible:self.caret_pos]
+        text = self.text[self.first_visible_col:self.caret_col]
         x = self.padding_left
         for (minx, maxx, miny, maxy, advance) in self.font.metrics(text):
             x += advance
         return x
 
+    def get_caret_pos(self):
+        lines = self.get_lines()
+        pos = 0
+        for i in range(self.caret_row):
+            pos += len(lines[i]) + 1
+        pos += self.caret_col
+        return pos
+
     def draw_caret(self):
         rect = self.form.screen.get_clip()
         self.form.screen.set_clip(self.rect())
-        pg.draw.line(self.form.screen, pg.Color('black'), (self.rect().left + self.caret_x(), self.rect().top + 4),
-                     (self.rect().left + self.caret_x(), self.rect().top + self.rect().height - 4))
+        pg.draw.line(self.form.screen, pg.Color('black'), (self.rect().left + self.caret_x(),
+                                                           self.rect().top + self.padding_top + self.font.get_linesize() * self.caret_row),
+                     (self.rect().left + self.caret_x(),
+                      self.rect().top + self.padding_top + self.font.get_linesize() * (self.caret_row + 1)))
         self.form.screen.set_clip(rect)
 
     def render(self):
         if self.form is not None:
             self.form.screen.set_clip(self.rect())
             pg.draw.rect(self.form.screen, self.background, self.rect())
-            pos = (self.rect().left + self.padding_left, self.rect().top + self.padding_top)
-            render_text_pos(screen=self.form.screen, font=self.font, text=self.text[self.first_visible:],
-                            color=self.color, pos=pos)
+            lines = self.get_lines()
+            for i in range(self.first_visible_row,len(lines)):
+                pos = (
+                self.rect().left + self.padding_left, self.rect().top + self.padding_top + (i-self.first_visible_row) * self.font.get_linesize())
+                render_text_pos(screen=self.form.screen, font=self.font, text=lines[i][self.first_visible_col:],
+                                color=self.color, pos=pos)
             pg.draw.rect(self.form.screen, pg.Color("black"), self.rect(), 1)
             if self.focused:
                 self.draw_caret()
             self.form.screen.set_clip(None)
 
-    def move_caret(self, x):
-        caret_pos = self.first_visible
+    def move_caret(self, pos):
+        caret_col = self.first_visible_col
         left = self.rect().left + self.padding_left
-        for (minx, maxx, miny, maxy, advance) in self.font.metrics(self.text[self.first_visible:]):
+        for (minx, maxx, miny, maxy, advance) in self.font.metrics(self.text[self.first_visible_col:]):
             left += advance
-            if left >= x:
+            if left >= pos[0]:
                 break
-            caret_pos += 1
-        self.set_caret_pos(caret_pos)
+            caret_col += 1
+        caret_row = (pos[1] - self.rect().top) // self.font.get_linesize()
 
-    def set_caret_pos(self, pos):
-        self.caret_pos = 0 if pos < 0 else len(self.text) if pos > len(self.text) else pos
-        while self.caret_x() > self.width - 2 and self.first_visible < len(self.text) - 1:
-            self.first_visible += 1
-        while self.first_visible > 0 and self.caret_pos - 1 < self.first_visible:
-            self.first_visible -= 1
+        self.set_caret(caret_row, caret_col)
+
+    def set_caret(self, caret_row, caret_col):
+        if caret_row < 0:
+            self.caret_row = 0
+        elif caret_row > len(self.get_lines()) - 1:
+            self.caret_row = len(self.get_lines()) - 1
+        else:
+            self.caret_row = caret_row
+        if caret_col < 0:
+            self.caret_col = 0
+        elif caret_col > len(self.get_lines()[self.caret_row]):
+            self.caret_col = len(self.get_lines()[self.caret_row])
+        else:
+            self.caret_col = caret_col
+        while self.caret_x() > self.width - 2 and self.first_visible_col < len(self.text) - 1:
+            self.first_visible_col += 1
+        while self.first_visible_col > 0 and self.caret_col - 1 < self.first_visible_col:
+            self.first_visible_col -= 1
+        while self.first_visible_row > self.caret_row:
+            self.first_visible_row -= 1
+        while self.first_visible_row + self.height // self.font.get_linesize() < self.caret_row:
+            self.first_visible_row += 1
         self.render()
 
     def mouse_down(self, event):
         super().mouse_down(event)
         if self.focused:
-            self.move_caret(event.pos[0])
+            self.move_caret(event.pos)
         else:
             self.render()
 
+    def on_key_down(self, event):
+        return True
+
     def key_down(self, event):
-        if event.key == pg.K_BACKSPACE and self.caret_pos > 0:
-            self.text = self.text[0:self.caret_pos - 1] + self.text[self.caret_pos:]
-            self.set_caret_pos(self.caret_pos - 1)
-        elif event.key == pg.K_DELETE and self.caret_pos < len(self.text):
-            self.text = self.text[0:self.caret_pos] + self.text[self.caret_pos + 1:]
-        elif event.key == pg.K_LEFT and self.caret_pos > 0:
-            self.set_caret_pos(self.caret_pos - 1)
-        elif event.key == pg.K_RIGHT and self.caret_pos < len(self.text):
-            self.set_caret_pos(self.caret_pos + 1)
+        if not self.on_key_down(event):
+            return
+        if event.key == pg.K_BACKSPACE and self.get_caret_pos() > 0:
+            self.text = self.text[0:self.get_caret_pos() - 1] + self.text[self.get_caret_pos():]
+            self.set_caret(self.caret_row, self.caret_col - 1)
+        elif event.key == pg.K_DELETE and self.get_caret_pos() < len(self.text):
+            self.text = self.text[0:self.get_caret_pos()] + self.text[self.get_caret_pos() + 1:]
+        elif event.key == pg.K_LEFT and self.caret_col > 0:
+            self.set_caret(self.caret_row, self.caret_col - 1)
+        elif event.key == pg.K_RIGHT and self.get_caret_pos() < len(self.text):
+            self.set_caret(self.caret_row, self.caret_col + 1)
         elif event.key == pg.K_HOME:
-            self.set_caret_pos(0)
+            self.set_caret(self.caret_row, 0)
         elif event.key == pg.K_END:
-            self.set_caret_pos(len(self.text))
+            self.set_caret(self.caret_row, len(self.get_lines()[self.caret_row]))
+        elif event.key == pg.K_UP:
+            self.set_caret(self.caret_row - 1, self.caret_col)
+        elif event.key == pg.K_DOWN:
+            self.set_caret(self.caret_row + 1, self.caret_col)
+        elif event.key == pg.K_RETURN:
+            self.text = self.text[0:self.get_caret_pos()] + "\n" + self.text[self.get_caret_pos():]
+            self.set_caret(self.caret_row + 1, 0)
         elif event.unicode and 32 <= ord(event.unicode) <= 126:
-            self.text = self.text[0:self.caret_pos] + event.unicode + self.text[self.caret_pos:]
-            self.set_caret_pos(self.caret_pos + 1)
+            self.text = self.text[0:self.get_caret_pos()] + event.unicode + self.text[self.get_caret_pos():]
+            self.set_caret(self.caret_row, self.caret_col + 1)
         self.render()
 
-class CheckBox(Control):
-    def __init__(self,form=None,left=0,top=0):
-        super().__init__(form=form,left=left,top=top)
-        self.checked=False
-        self.width=20
-        self.height=20
 
-    def mouse_click(self,event):
-        self.checked=not self.checked
+class CheckBox(Control):
+    def __init__(self, form=None, left=0, top=0):
+        super().__init__(form=form, left=left, top=top)
+        self.checked = False
+        self.width = 20
+        self.height = 20
+
+    def mouse_click(self, event):
+        self.checked = not self.checked
         self.render()
 
     def render(self):
-        pg.draw.rect(self.form.screen, pg.Color('white'), self.rect(),0)
-        pg.draw.rect(self.form.screen,pg.Color('black'),self.rect(),1)
+        pg.draw.rect(self.form.screen, pg.Color('white'), self.rect(), 0)
+        pg.draw.rect(self.form.screen, pg.Color('black'), self.rect(), 1)
         if self.checked:
-            pg.draw.line(self.form.screen,pg.Color('black'),(self.rect().left,self.rect().top),
-                         (self.rect().left+self.rect().width-1,self.rect().top+self.rect().height-1),1)
-            pg.draw.line(self.form.screen, pg.Color('black'), (self.rect().left, self.rect().top+self.rect().height-1),
-                         (self.rect().left + self.rect().width-1, self.rect().top), 1)
+            pg.draw.line(self.form.screen, pg.Color('black'), (self.rect().left, self.rect().top),
+                         (self.rect().left + self.rect().width - 1, self.rect().top + self.rect().height - 1), 1)
+            pg.draw.line(self.form.screen, pg.Color('black'),
+                         (self.rect().left, self.rect().top + self.rect().height - 1),
+                         (self.rect().left + self.rect().width - 1, self.rect().top), 1)
+
+
+class RadioButton(Control):
+    def __init__(self, form=None, left=0, top=0, group_id='default'):
+        super().__init__(form=form, left=left, top=top)
+        self.checked = False
+        self.width = 20
+        self.height = 20
+        self.group_id = group_id
+
+    def mouse_click(self, event):
+        self.checked = True
+        for control in self.form.controls:
+            if control != self and isinstance(control,
+                                              RadioButton) and control.checked and control.group_id == self.group_id:
+                control.checked = False
+                control.render()
+        self.render()
+
+    def render(self):
+        pg.gfxdraw.filled_circle(self.form.screen, self.rect().left + self.width // 2,
+                                 self.rect().top + self.height // 2, 10, pg.Color('white'))
+        pg.gfxdraw.circle(self.form.screen, self.rect().left + self.width // 2,
+                          self.rect().top + self.height // 2, 10, pg.Color('black'))
+        if self.checked:
+            pg.gfxdraw.circle(self.form.screen, self.rect().left + self.width // 2,
+                              self.rect().top + self.height // 2, 5, pg.Color('black'))
+            pg.gfxdraw.filled_circle(self.form.screen, self.rect().left + self.width // 2,
+                                     self.rect().top + self.height // 2, 5, pg.Color('black'))
+
 
 class PyForm(Control):
     title_height = 20
@@ -365,11 +447,13 @@ if __name__ == "__main__":
     pg.font.init()
     screen = pg.display.set_mode((400, 400))
     pg.key.set_repeat(500, 100)
-    form1 = PyForm(screen, 10, 10, 200, 200, title="Form1")
-    form1.add_control(Button(width=100, height=20, left=20, top=10,text="ABCgyl"))
+    form1 = PyForm(screen, 10, 10, 300, 200, title="Form1")
+    form1.add_control(Button(width=100, height=20, left=20, top=10, text="ABCgyl"))
     form1.add_control(Label(width=100, height=20, top=50, left=20, text="ABCgyl"))
-    form1.add_control(TextBox(width=100, height=20, top=100,left=20, text="ABCgyl"))
-    form1.add_control(CheckBox(top=150,left=20))
+    form1.add_control(TextBox(width=100, height=60, top=100, left=20, text="ABCgyl\r\n1234", multilines=False))
+    form1.add_control(CheckBox(top=150, left=20))
+    form1.add_control(RadioButton(top=150, left=50))
+    form1.add_control(RadioButton(top=150, left=100))
 
     form1.open()
     running = True
